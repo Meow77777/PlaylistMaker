f8d4993ca8bda7322565285856a878544e794ac1
Meow <egor.football11@yandex.ru>
2024-01-10 02:01:59 +0300

    commit1

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.practicum.playlistmaker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val poisk = findViewById<Button>(R.id.poisk_button)
+        val mediateka = findViewById<Button>(R.id.mediateka_button)
+        val settings = findViewById<Button>(R.id.settings_button)
+
+        val poiskIntent = Intent(this, PoiskActivity::class.java)
+        val imageClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                startActivity(poiskIntent)
+            }
+        }
+        poisk.setOnClickListener(imageClickListener)
+
+        mediateka.setOnClickListener {
+            val mediatekaIntent = Intent(this, MediatekaActivity::class.java)
+            startActivity(mediatekaIntent)
+        }
+
+        settings.setOnClickListener {
+            val settingsIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(settingsIntent)
+        }
     }
 }
