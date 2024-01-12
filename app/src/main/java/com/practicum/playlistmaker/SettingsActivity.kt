00a44ed0ababb157ae586cf101465f3d59833d1d
Meow <egor.football11@yandex.ru>
2024-01-13 00:16:16 +0300

    commit3 касательно обводки вокруг кнопки Назад - там моя ошибки, я все замечания смотрел по гиту только и забыл, что ещё на сайте пишется ... это было единственное крит.замечание, я его исправил, теперь всё приемлемо должно быть

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -4,13 +4,15 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        val buttonSettingsBack = findViewById<Button>(R.id.settingsBack)
+        val buttonSettingsBack = findViewById<ImageView>(R.id.settingsBack)
 
         buttonSettingsBack.setOnClickListener {
             finish()
