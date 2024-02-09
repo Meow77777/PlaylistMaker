56275dba6a87cd4946b492be97643f659e68d0a8
Egor Skrypchenko <egor.football11@yandex.ru>
2024-02-09 23:51:45 +0300

    Commit2

--- app/src/main/java/com/practicum/playlistmaker/SongSearchViewHolder.kt
+++ app/src/main/java/com/practicum/playlistmaker/SongSearchViewHolder.kt
@@ -1,5 +1,7 @@
 package com.practicum.playlistmaker
 
+import android.content.Context
+import android.util.TypedValue
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
@@ -10,8 +12,8 @@ import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 class SongSearchViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private val trackNameView: TextView
     private val artistNameView: TextView
-    val trackTimeView: TextView
-    val artworkUrl100View: ImageView
+    private val trackTimeView: TextView
+    private val artworkUrl100View: ImageView
 
     init {
         trackNameView = itemView.findViewById(R.id.SongNameList)
@@ -20,12 +22,23 @@ class SongSearchViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         artworkUrl100View = itemView.findViewById(R.id.SongImageList)
     }
 
+
     fun bind(model: Track) {
         trackNameView.text = model.trackName
         artistNameView.text = model.artistName
         trackTimeView.text = model.trackTime
         Glide.with(itemView).load(model.artworkUrl100).centerCrop()
-            .placeholder(R.drawable.placeholder).transform(RoundedCorners(2))
+            .placeholder(R.drawable.placeholder)
+            .transform(RoundedCorners(dpToPx(2f, itemView.context)))
             .into(artworkUrl100View)
+
+    }
+
+    private fun dpToPx(dp: Float, context: Context): Int {
+        return TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            dp,
+            context.resources.displayMetrics
+        ).toInt()
     }
 }
