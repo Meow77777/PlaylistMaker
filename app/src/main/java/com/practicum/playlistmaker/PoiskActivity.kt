fa927b68673ad9b06ede2e2a7b71c7bda79fe7fe
Egor Skrypchenko <egor.football11@yandex.ru>
2024-02-19 22:23:56 +0300

    Commit1/Sprint11

--- app/src/main/java/com/practicum/playlistmaker/PoiskActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/PoiskActivity.kt
@@ -1,64 +1,113 @@
 package com.practicum.playlistmaker
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 
 class PoiskActivity : AppCompatActivity() {
+
+    private val ITUNES_URL = "https://itunes.apple.com"
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(ITUNES_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    private val iTunesService = retrofit.create(TrackApi::class.java)
+    private val tracks = ArrayList<Track>()
+    private val adapter = SongSearchAdapter(tracks)
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_poisk)
         val recycler = findViewById<RecyclerView>(R.id.listView)
-        val tracksList = mutableListOf(
-            Track(
-                "Smells Like Teen Spirit",
-                "Nirvana",
-                "5:01",
-                "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"
-            ),
-            Track(
-                "Billie Jean",
-                "Michael Jackson",
-                "4:35",
-                "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg",
-            ),
-            Track(
-                "Stayin' Alive",
-                "Bee Gees",
-                "4:10",
-                "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg",
-            ),
-            Track(
-                "Whole Lotta Love",
-                "Led Zeppelin",
-                "5:33",
-                "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg",
-            ),
-            Track(
-                "Sweet Child O'Mine",
-                "Guns N' Roses",
-                "5:03",
-                "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg",
-            )
-        )
-        recycler.adapter = SongSearchAdapter(tracksList)
-
-
         val buttonSearchBack = findViewById<ImageView>(R.id.settingsBackFromSearch)
         val editText = findViewById<EditText>(R.id.editText)
         val buttonSearchDelete = findViewById<ImageView>(R.id.deleteSearchButton)
+        val placeholderImage = findViewById<ImageView>(R.id.placeholderImage)
+        val placeholderText = findViewById<TextView>(R.id.placeholderText)
+        val placeholderResetButton = findViewById<Button>(R.id.placeholderResetButton)
 
+        placeholderImage.isVisible = false
+        placeholderText.isVisible = false
+        placeholderResetButton.isVisible = false
+
+        recycler.adapter = adapter
+        fun searchSong() {
+            if (editText.text.isNotEmpty()) {
+                iTunesService.search(editText.text.toString()).enqueue(object :
+                    Callback<TrackResponse> {
+
+                    @SuppressLint("NotifyDataSetChanged")
+                    override fun onResponse(
+                        call: Call<TrackResponse>,
+                        response: Response<TrackResponse>
+                    ) {
+                        if (response.code() == 200) {
+                            tracks.clear()
+                            placeholderImage.isVisible = false
+                            placeholderText.isVisible = false
+                            if (response.body()?.results?.isNotEmpty() == true) {
+                                tracks.addAll(response.body()?.results!!)
+                                adapter.notifyDataSetChanged()
+                            }
+                            if (tracks.isEmpty()) {
+                                placeholderText.isVisible = true
+                                placeholderImage.isVisible = true
+                                placeholderText.text = getString(R.string.nothingFound)
+                                placeholderImage.setBackgroundResource(R.drawable.nothing_found)
+                            }
+                        } else {
+                            placeholderText.isVisible = true
+                            placeholderImage.isVisible = true
+                            placeholderText.text = getString(R.string.internetProblem)
+                            placeholderImage.setBackgroundResource(R.drawable.no_internet)
+                        }
+                    }
+
+
+                    override fun onFailure(call: Call<TrackResponse>, t: Throwable) {
+                        placeholderText.isVisible = true
+                        placeholderImage.isVisible = true
+                        placeholderResetButton.isVisible = true
+                        placeholderText.text = getString(R.string.internetProblem)
+                        placeholderImage.setBackgroundResource(R.drawable.no_internet)
+                    }
+                })
+            }
+        }
+
+        editText.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                searchSong()
+            }
+            false
+        }
+
+        placeholderResetButton.setOnClickListener {
+            searchSong()
+            placeholderImage.isVisible = false
+            placeholderText.isVisible = false
+            placeholderResetButton.isVisible = false
+        }
         editText.isFocusable = true
 
         buttonSearchDelete.isVisible = false
@@ -67,12 +116,16 @@ class PoiskActivity : AppCompatActivity() {
             editText.setText(savedInstanceState.getString(EDIT_TEXT, enterEditText))
         }
 
-        buttonSearchBack.setOnClickListener {
+        buttonSearchBack.setOnClickListener()
+        {
             finish()
         }
 
-        buttonSearchDelete.setOnClickListener {
+        buttonSearchDelete.setOnClickListener()
+        {
             editText.text.clear()
+            tracks.clear()
+            adapter.notifyDataSetChanged()
         }
 
         val simpleTextWatcher = object : TextWatcher {
--- app/src/main/java/com/practicum/playlistmaker/SongSearchViewHolder.kt
+++ app/src/main/java/com/practicum/playlistmaker/SongSearchViewHolder.kt
@@ -8,6 +8,8 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class SongSearchViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private val trackNameView: TextView
@@ -26,7 +28,7 @@ class SongSearchViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     fun bind(model: Track) {
         trackNameView.text = model.trackName
         artistNameView.text = model.artistName
-        trackTimeView.text = model.trackTime
+        trackTimeView.text = SimpleDateFormat("mm:ss", Locale.getDefault()).format(model.trackTimeMillis.toLong())
         Glide.with(itemView).load(model.artworkUrl100).centerCrop()
             .placeholder(R.drawable.placeholder)
             .transform(RoundedCorners(dpToPx(2f, itemView.context)))
@@ -41,4 +43,5 @@ class SongSearchViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
             context.resources.displayMetrics
         ).toInt()
     }
+
 }
